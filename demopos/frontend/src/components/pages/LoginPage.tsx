56036224f91e80119c7003ff1eb51ab9fe660dc5
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-04-22 14:20:35 +0700

    1234

--- demopos/frontend/src/components/pages/LoginPage.tsx
+++ demopos/frontend/src/components/pages/LoginPage.tsx
@@ -1,7 +1,26 @@
+import { Button } from "@mui/material";
 import React from "react";
 
-type Props = {};
+export default function LoginPage() {
+  return (
+    <div>
+      <b>Login</b>
+      <br />
+      <input type="text" name="username" id="username" placeholder="Username" />
 
-export default function LoginPage({}: Props) {
-  return <div>LoginPage</div>;
+      <br />
+      <input type="text" name="password" id="password" placeholder="Password" />
+
+      <br />
+
+      <Button fullWidth variant="contained">
+        Login
+      </Button>
+      <br />
+      <br />
+      <Button fullWidth variant="text">
+        Clear
+      </Button>
+    </div>
+  );
 }
