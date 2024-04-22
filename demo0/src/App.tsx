fa1e5fe33b6c6d450f18f4e2c87aa52af40a4477
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-04-22 10:51:56 +0700

    1234

--- demo0/src/App.tsx
+++ demo0/src/App.tsx
@@ -1,20 +1,9 @@
-import { useState } from "react";
-
 export default function App() {
-  // let count = 0 // non-side effect variable
-  const [count1, setCount1] = useState(0); // state
-
   // JSX => Javascript + XML
   return (
     <>
-      <div>Lek CodeMobiles {count1}</div>
-      <button
-        onClick={() => {
-          setCount1(count1 + 1);
-        }}
-      >
-        Add
-      </button>
+      <div>Lek CodeMobiles {}</div>
+      <button onClick={() => {}}>Add</button>
     </>
   );
 }
