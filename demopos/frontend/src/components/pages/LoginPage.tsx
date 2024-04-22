af2f08ad0958d57750b3adcc58f762f6ccde2d66
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-04-22 15:18:42 +0700

    1234

--- demopos/frontend/src/components/pages/LoginPage.tsx
+++ demopos/frontend/src/components/pages/LoginPage.tsx
@@ -1,26 +1,19 @@
 import { Button } from "@mui/material";
 import React from "react";
-import { Controller, useForm } from "react-hook-form";
 
 type User = {
   username: string;
   password: string;
 };
 export default function LoginPage() {
-  const { control, handleSubmit, reset } = useForm<User>({ defaultValues: { username: "lek", password: "1234" } });
-
   return (
-    <form
-      onSubmit={handleSubmit((result) => {
-        alert(JSON.stringify(result));
-      })}
-    >
+    <form>
       <b>Login</b>
       <br />
-      <Controller control={control} name="username" render={({ field }) => <input type="text" placeholder="Username" {...field} />} />
+      <input type="text" name="username" id="username" placeholder="Username" onChange={(e) => {}} />
 
       <br />
-      <Controller control={control} name="password" render={({ field }) => <input type="text" placeholder="Password" {...field} />} />
+      <input type="password" name="password" id="password" placeholder="Password" onChange={(e) => {}} />
 
       <br />
 
@@ -29,7 +22,7 @@ export default function LoginPage() {
       </Button>
       <br />
       <br />
-      <Button type="button" fullWidth variant="text" onClick={() => reset()}>
+      <Button type="button" fullWidth variant="text">
         Clear
       </Button>
     </form>
