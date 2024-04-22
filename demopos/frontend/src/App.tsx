d40aa136afce1024b17efd511bae6669a44acc39
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-04-22 13:13:14 +0700

    1234

--- demopos/frontend/src/App.tsx
+++ demopos/frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Button } from "@mui/material";
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./components/pages/LoginPage";
+import RegisterPage from "./components/pages/RegisterPage";
 
 type Props = {};
 
 export default function App({}: Props) {
-  return <div>
-
-    <Routes>
-      
-    </Routes>
-  </div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </div>
+  );
 }
