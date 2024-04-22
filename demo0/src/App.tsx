c00e3f5c7aa3fd087017dad69c970886407e39ca
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-04-22 11:07:45 +0700

    1234

--- demo0/src/App.tsx
+++ demo0/src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 
 export default function App() {
-  const tmp0 = true // implicit declation
+  const tmp0 = true; // implicit declation
   const tmp1: number = 0; // explicit declation
-  const tmp2: string = "";
+  const tmp2: string = "hey";
+
   const [count, setCount] = useState<number>(0);
 
   return (
