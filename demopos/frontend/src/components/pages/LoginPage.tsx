27539c3b47e128cb142a0cee38197fa165477621
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-04-22 14:30:41 +0700

    1234

--- demopos/frontend/src/components/pages/LoginPage.tsx
+++ demopos/frontend/src/components/pages/LoginPage.tsx
@@ -6,15 +6,25 @@ type User = {
   password: string;
 };
 export default function LoginPage() {
-  const account: User = { username: "admin", password: "1234" };
+  let account: User = { username: "admin", password: "1234" };
   return (
     <div>
       <b>Login</b>
       <br />
-      <input type="text" name="username" id="username" placeholder="Username" />
+      <input
+        type="text"
+        name="username"
+        id="username"
+        placeholder="Username" 
+        value={account.username}
+        onChange={(e) => {
+          console.log(e.target.value);
+          account = { ...account, username: e.target.value };
+        }}
+      />
 
       <br />
-      <input type="text" name="password" id="password" placeholder="Password" />
+      <input type="password" name="password" id="password" placeholder="Password" value={account.password} />
 
       <br />
 
