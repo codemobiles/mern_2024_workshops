c625c753cc612edf49b554476f9cebb5a7ac9758
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-04-23 15:30:46 +0700

    1234

--- demopos/backend/src/index.ts
+++ demopos/backend/src/index.ts
@@ -2,12 +2,14 @@ import * as express from "express";
 import { Request, Response } from "express";
 import { AppDataSource } from "./data-source";
 import { Routes } from "./routes";
+import * as cors from "cors";
 
 AppDataSource.initialize()
   .then(async () => {
     // create express app
     const app = express();
     app.use(express.json());
+    app.use(cors());
 
     // register express routes from defined application routes
     Routes.forEach((route) => {
