b70e7a0b654608e2431e49df6a24c13ec664f94f
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-04-23 15:13:38 +0700

    1234

--- demopos/backend/src/index.ts
+++ demopos/backend/src/index.ts
@@ -8,10 +8,10 @@ AppDataSource.initialize()
     // create express app
     const app = express();
     app.use(express.json());
-
+ 
     // register express routes from defined application routes
     Routes.forEach((route) => {
-      (app as any)[route.method]("api/v2/" + route.route, (req: Request, res: Response, next: Function) => {
+      (app as any)[route.method]("/api/v2" + route.route, (req: Request, res: Response, next: Function) => {
         const result = new (route.controller as any)()[route.action](req, res, next);
         if (result instanceof Promise) {
           result.then((result) => (result !== null && result !== undefined ? res.send(result) : undefined));
