4ee891a1a80eaaaea3fa394e7cd72fc4e4fe1db9
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-04-24 10:22:28 +0700

    1234

--- demopos/frontend/src/main.tsx
+++ demopos/frontend/src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "store/store.ts";
+import store from "@/store/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
