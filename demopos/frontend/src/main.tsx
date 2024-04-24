6456bdd6edd697707baf1f1114e45d44985ada23
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-04-24 10:07:46 +0700

    1234

--- demopos/frontend/src/main.tsx
+++ demopos/frontend/src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "store/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     {/* Hoc -> Higher Order Component */}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
