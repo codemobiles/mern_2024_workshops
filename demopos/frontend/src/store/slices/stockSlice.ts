69ff49aa50a1d7eda00875bd8096cce07e9e53f4
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-04-24 15:32:05 +0700

    1234

--- demopos/frontend/src/store/slices/stockSlice.ts
+++ demopos/frontend/src/store/slices/stockSlice.ts
@@ -1,15 +1,17 @@
 import { Product } from "@/types/product.type";
-import { createSlice } from "@reduxjs/toolkit";
+import { httpClient } from "@/utils/HttpClient";
+import { server } from "@/utils/constants";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export interface StockState {
   stockAllResult: Product[];
   stockOneResult: Product | null;
 }
 
-export const getProducts = ()=>{
-    
-}
-
+export const getProducts = createAsyncThunk("stock/getProducts", async () => {
+  const result = await httpClient.get<Product[]>(server.PRODUCT_URL);
+  return result.data;
+});
 
 const initialState: StockState = {
   stockAllResult: [],
@@ -18,7 +20,7 @@ const initialState: StockState = {
 
 const stockSlice = createSlice({
   name: "stock",
-  initialState ,
+  initialState,
   reducers: {},
 });
 
