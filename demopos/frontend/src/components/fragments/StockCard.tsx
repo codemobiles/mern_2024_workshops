895e18e0e5cae5135fdaeed20f33760743089083
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-04-25 09:26:29 +0700

    1234

---
+++ demopos/frontend/src/components/fragments/StockCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Props = {
+  icon: any;
+  title: string;
+  subtitle: string;
+  color: string;
+};
+
+export default function StockCard({}: Props) {
+  return <div>StockCard</div>;
+}
