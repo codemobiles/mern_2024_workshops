69239501c48a75b0f818235fb55649edc8b945b4
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-04-25 11:39:02 +0700

    1234

--- demopos/frontend/src/components/pages/StockEditPage.tsx
+++ demopos/frontend/src/components/pages/StockEditPage.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useMatch } from "react-router-dom";
 
 type Props = {};
 
 export default function StockEditPage({}: Props) {
   const match = useMatch("/stock/edit/:id");
 
-  
+  const loadData = () => {
+    const product_id = match?.params.id;
+    setTimeout(() => {
+      alert("productId " + product_id);
+    }, 2000);
+  };
 
+  useEffect(() => {
+    loadData();
+  }, []);
   return <div>StockEditPage</div>;
 }
