8420ed2ff6db448fbf4c2619448c6714e954a3c2
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-04-25 11:36:26 +0700

    1234

--- demopos/frontend/src/components/pages/StockEditPage.tsx
+++ demopos/frontend/src/components/pages/StockEditPage.tsx
@@ -3,5 +3,9 @@ import React from "react";
 type Props = {};
 
 export default function StockEditPage({}: Props) {
+  const match = useMatch("/stock/edit/:id");
+
+  
+
   return <div>StockEditPage</div>;
 }
