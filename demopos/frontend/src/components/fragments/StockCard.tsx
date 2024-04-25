688ed05671003f02fe463d08d60f038c4f04ea4b
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-04-25 13:37:53 +0700

    1234

--- demopos/frontend/src/components/fragments/StockCard.tsx
+++ demopos/frontend/src/components/fragments/StockCard.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import React from "react";
 
 type Props = {
@@ -8,10 +8,11 @@ type Props = {
   color: string;
 };
 
-export default function StockCard(props: Props) {
+export default function StockCard(props: { icon: any; title: string; subtitle: string; color: string }) {
   return (
     <Stack>
       StockCard
+      <Typography color={props.color}>{props.title}</Typography>
       <props.icon />
     </Stack>
   );
