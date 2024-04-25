0116231ad43d390495283ed074f295d4868bab65
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-04-25 10:00:44 +0700

    1234

--- demopos/frontend/src/components/pages/StockCreatePage.tsx
+++ demopos/frontend/src/components/pages/StockCreatePage.tsx
@@ -1,9 +1,10 @@
-import { Box, Button, Card, Stack, TextField } from "@mui/material";
+import { Box, Button, Card, Stack, TextField, Typography } from "@mui/material";
 
 export default function StockCreatePage() {
   return (
     <div>
       <Card elevation={10} sx={{ p: 20 }}>
+        <Typography variant="h3">Product Creation</Typography>
         <form>
           <TextField placeholder="Name" fullWidth sx={{ py: 1 }} />
           <TextField placeholder="Price" fullWidth sx={{ py: 1 }} />
