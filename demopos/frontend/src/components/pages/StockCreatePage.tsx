2a555e587efe2680ff362b3e57dca6c210041c23
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-04-25 10:18:57 +0700

    1234

--- demopos/frontend/src/components/pages/StockCreatePage.tsx
+++ demopos/frontend/src/components/pages/StockCreatePage.tsx
@@ -1,16 +1,31 @@
 import { Product } from "@/types/product.type";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { Box, Button, Card, Stack, TextField, Typography } from "@mui/material";
 import { Controller, useForm } from "react-hook-form";
+import * as Yup from "yup";
+
+const formValidateSchema = Yup.object().shape({
+  // username: Yup.string().email("Invalid email address").required("Email is required").trim(),
+  name: Yup.string().required("Product name is required").trim(),
+  stock: Yup.number().min(1).required("Minimum stock is 1"),
+  price: Yup.number().min(1).required("Minimum price is 1"),
+});
 
 export default function StockCreatePage() {
-  const { control, handleSubmit } = useForm<Product>({
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     defaultValues: { name: "Your Product", stock: 0, price: 0 },
+    resolver: yupResolver(formValidateSchema),
   });
+
   return (
     <div>
       <Card elevation={10} sx={{ p: 20 }}>
         <Typography variant="h3">Product Creation</Typography>
-        <form>
+        <form onSubmit={handleSubmit((result) => alert(JSON.stringify(result)))}>
           {/* Name */}
           <Controller control={control} name="name" render={({ field }) => <TextField {...field} placeholder="Name" fullWidth sx={{ py: 1 }} />} />
 
@@ -22,7 +37,9 @@ export default function StockCreatePage() {
           <Stack direction={"row"} sx={{ pt: 8 }}>
             <Box className="grow" />
             <Button>Cancel</Button>
-            <Button variant="contained">Create</Button>
+            <Button type="submit" variant="contained">
+              Create
+            </Button>
           </Stack>
         </form>
       </Card>
