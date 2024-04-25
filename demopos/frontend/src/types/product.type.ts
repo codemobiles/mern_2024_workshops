4d6983efd1758538ae7005b23fd95cc1f0692fec
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-04-25 10:31:31 +0700

    1234

--- demopos/frontend/src/types/product.type.ts
+++ demopos/frontend/src/types/product.type.ts
@@ -1,10 +1,14 @@
 export interface Product {
-  created?: Date;
-  __v?: number;
   _id?: string;
   product_id?: number;
-  name: string;
   image?: string;
-  stock: number | string;
-  price: number | string;
+  name: string;
+  stock: number;
+  qty?: number;
+  price: number;
+  created?: Date;
+  __v?: number;
+
+  file?: any;
+  file_obj?: string;
 }
