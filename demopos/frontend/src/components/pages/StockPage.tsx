819f0ad2787e11c5571281b7848bc9c5c1e8e709
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-04-25 11:30:22 +0700

    1234

--- demopos/frontend/src/components/pages/StockPage.tsx
+++ demopos/frontend/src/components/pages/StockPage.tsx
@@ -201,29 +201,47 @@ const Stock = () => {
     setOpenDialog(false);
   };
 
+  const showMyDialog = () => {
+    return (
+      <Dialog
+        open={openMyDialog}
+        onClose={() => {
+          setOpenMyDialog(false);
+        }}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => {
+              setOpenMyDialog(false);
+            }}
+          >
+            Disagree
+          </Button>
+          <Button
+            onClick={() => {
+              setOpenMyDialog(false);
+            }}
+            autoFocus
+          >
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  };
+
   const showDialog = () => {
     if (selectedProduct === null) {
       return "";
     }
-
-    const showMyDialog = () => {
-      return (
-        <Dialog open={openMyDialog} onClose={handleClose} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
-          <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose}>Disagree</Button>
-            <Button onClick={handleClose} autoFocus>
-              Agree
-            </Button>
-          </DialogActions>
-        </Dialog>
-      );
-    };
 
     return (
       <Dialog open={openDialog} keepMounted aria-labelledby="alert-dialog-slide-title" aria-describedby="alert-dialog-slide-description">
@@ -292,6 +310,7 @@ const Stock = () => {
         }}
       />
       {showDialog()}
+      {showMyDialog()}
     </Box>
   );
 };
