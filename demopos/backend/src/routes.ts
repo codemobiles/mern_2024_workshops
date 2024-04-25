688bb7e09368bcdee1cdf5b51d207ba1d7b2fc30
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-04-25 11:05:14 +0700

    1234

--- demopos/backend/src/routes.ts
+++ demopos/backend/src/routes.ts
@@ -16,12 +16,42 @@ export const authRoutes = [
   },
 ];
 
-export const productRoutes = [
+const productRoutes = [
   {
     method: "get",
     route: "/product",
     controller: ProductController,
     action: "all",
+  },
+  {
+    method: "get",
+    route: "/product/id/:product_id",
+    controller: ProductController,
+    action: "one",
+  },
+  {
+    method: "post",
+    route: "/product",
+    controller: ProductController,
+    action: "add",
+  },
+  {
+    method: "put",
+    route: "/product",
+    controller: ProductController,
+    action: "update",
+  },
+  {
+    method: "delete",
+    route: "/product/id/:product_id",
+    controller: ProductController,
+    action: "remove",
+  },
+  {
+    method: "get",
+    route: "/product/name/:name",
+    controller: ProductController,
+    action: "allLike",
   },
 ];
 
