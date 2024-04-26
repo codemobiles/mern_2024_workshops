a00396669c07d22fce81ee1996bd8fd81100c243
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2024-04-26 09:32:52 +0700

    1234

--- demopos/frontend/src/components/pages/TransactionPage.tsx
+++ demopos/frontend/src/components/pages/TransactionPage.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+import * as React from "react";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
-type Props = {};
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "firstName", headerName: "First name", width: 130 },
+  { field: "lastName", headerName: "Last name", width: 130 },
+  {
+    field: "age",
+    headerName: "Age",
+    type: "number",
+    width: 90,
+  },
+];
 
-export default function TransactionPage({}: Props) {
-  return <div>TransactionPage</div>;
+const rows = [
+  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: 30 },
+  { id: 6, lastName: "Melisandre", firstName: "null", age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+];
+
+export default function TransactionPage() {
+  return (
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
+    </div>
+  );
 }
